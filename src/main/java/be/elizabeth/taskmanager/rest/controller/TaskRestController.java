a517c3dea9d1408237262f2076cee10c66c732ef
LisaRamaekers <lisa.ramaekers@student.ucll.be>
2020-06-05 04:56:55 +0200

    circleci config - auth fix // rest funct

--- src/main/java/be/elizabeth/taskmanager/rest/controller/TaskRestController.java
+++ src/main/java/be/elizabeth/taskmanager/rest/controller/TaskRestController.java
@@ -1,4 +1,29 @@
 package be.elizabeth.taskmanager.rest.controller;
 
+import be.elizabeth.taskmanager.DTO.TaskDTO;
+import be.elizabeth.taskmanager.service.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
 public class TaskRestController {
+    private final TaskService taskService;
+
+    public TaskRestController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping("/tasks")
+    @ResponseBody
+    public List<TaskDTO> getTasks() {
+        return taskService.getAll();
+    }
+
+    @PostMapping("/task")
+    public void createNewTask(@RequestBody @Valid TaskDTO taskDTO){
+        taskService.add(taskDTO);
+    }
 }
